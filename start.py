17e95ab2be6e49b4b1dd37edf416f82cc829bb57
felix-janakow <felix.janakow@ibm.com>
2025-05-09 14:39:09 +0200

    adjust start.py function

--- start.py
+++ start.py
@@ -11,7 +11,7 @@ def collect_user_input():
     source_bucket = input("SOURCE_BUCKET: ")
     destination_bucket = input("DESTINATION_BUCKET: ")
     iam_api_key = input("IAM_API_KEY: ")
-    account_id = input("ACCOUNT_ID: ")
+    # account_id = input("ACCOUNT_ID: ")
     region = input("REGION: ")
 
     # Speichert die Eingaben in der .env-Datei
@@ -19,23 +19,22 @@ def collect_user_input():
         env_file.write(f"SOURCE_BUCKET={source_bucket}\n")
         env_file.write(f"DESTINATION_BUCKET={destination_bucket}\n")
         env_file.write(f"IAM_API_KEY={iam_api_key}\n")
-        env_file.write(f"ACCOUNT_ID={account_id}\n")
+        # env_file.write(f"ACCOUNT_ID={account_id}\n")
         env_file.write(f"REGION={region}\n")
 
     print("\nKonfiguration wurde in der .env-Datei gespeichert.")
 
 def execute_file_copy():
-    """Führt die file_copy.py aus."""
+    # Führt die file_copy.py aus
     script_path = os.path.join(os.path.dirname(__file__), "file_copy.py")
-    subprocess.run(["python3", script_path])  # Ändere 'python' zu 'python3'
+    subprocess.run(["python3", script_path])
 
 def main():
-    """Hauptfunktion, die die Logik steuert."""
     # Prüfen, ob die .env-Datei existiert
     if os.path.exists(ENV_FILE_PATH):
         print("Eine .env-Datei wurde gefunden.")
-        choice = input("Möchten Sie neue Eingaben machen? (ja/nein): ").strip().lower()
-        if choice == "ja":
+        choice = input("Möchten Sie neue Eingaben machen? (y/N): ").strip().lower()
+        if choice == "y":
             collect_user_input()
         else:
             print("Vorhandene Werte aus der .env-Datei werden verwendet.")
